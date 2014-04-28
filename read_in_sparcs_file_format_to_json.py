60deb38af5c127771069a1efcc1076cb84044fb4
Janos Hajagos <risk.limits@gmail.com>
2014-04-28 13:22:05 -0400

    Simple working example of export

---
+++ read_in_sparcs_file_format_to_json.py
@@ -0,0 +1,26 @@
+__author__ = 'janos'
+
+import csv
+import json
+
+def main(csv_file_name):
+
+    with open(csv_file_name) as f:
+        r = csv.DictReader(f)
+        list_of_dicts = []
+        for row_dict in r:
+            row_dict.pop("")
+            list_of_dicts += [row_dict]
+
+    number_filler_fields = 1
+    for row_dict in list_of_dicts:
+        if row_dict["Field Label"] == "Filler":
+            row_dict["Field Label"] = "Filler %s" % number_filler_fields
+            number_filler_fields += 1
+
+    with open(csv_file_name + ".json", "w") as fw:
+        json.dump(list_of_dicts, fw,  sort_keys=True, indent=4, separators=(',', ': '))
+
+
+if __name__ == "__main__":
+    main("SPARCS OP Format_LIMITED.csv")
---
+++ transform_sparcs_file_into_db_loadable_csv.py
@@ -0,0 +1,41 @@
+"""
+The goal is to import SPARCS LDS outpatient database into a CSV which can be loaded into a
+database.
+
+"""
+
+__author__ = 'janos'
+
+import csv
+import json
+
+
+def main(sparcs_dat_file, sparcs_dat_file_json_structure):
+
+    with open(sparcs_dat_file_json_structure, "r") as fj:
+        sparcs_layout = json.load(fj)
+
+
+    header = []
+    for column_detail_in in sparcs_layout:
+        header += [column_detail_in["Field Label"]]
+    sparcs_dat_csv_file = sparcs_dat_file + ".csv"
+    with open(sparcs_dat_file, "r") as fd:
+        with open(sparcs_dat_csv_file, "w") as fw:
+
+            csv_writer = csv.writer(fw)
+            csv_writer.writerow(header)
+
+            starting_positions = [(int(sl["From"]) - 1, int(sl["Size"])) for sl in sparcs_layout]
+            print(starting_positions)
+
+            row_to_write = []
+            for line in fd:
+                for start_pos in starting_positions:
+                    row_to_write += [line[start_pos[0]:start_pos[0] + start_pos[1]].strip()]
+                csv_writer.writerow(row_to_write)
+
+
+
+if __name__ == "__main__":
+    main("/Volumes/untitled/LIMITEDOP12p1.DAT.sample.sample", "SPARCS OP Format_LIMITED.csv.json")
